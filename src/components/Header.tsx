3db428db643c7e9ac6307d1118ba4b7e51e3dbb2
Harshit Bakrania <harshit.bakrania@gmail.com>
2024-11-19 21:04:48 +0530

    hero component & assets added

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,7 @@
 import LogoIcon from "@/assets/logo.svg";
 import MenuIcon from "@/assets/icon-menu.svg"
 import Image from "next/image";
+import { Button } from "./Button";
 
 export const Header = () => {
   return (
@@ -19,14 +20,7 @@ export const Header = () => {
             </nav>
           </div>
           <div className="flex gap-4 items-center">
-            <button className="relative py-2 px-3 rounded-lg font-medium text-sm bg-gradient-to-b from-[#190d2e] to-[#4a208a] shadow-[0px_0px_12px_#8c45ff]">
-            <div className="absolute inset-0">
-                <div className="border rounded-lg border-white/20 absolute inset-0 [mask-image: linear-gradient(to_bottom,black,transparent)]"></div>
-                <div className="border rounded-lg absolute inset-0 border-white/40 [mask-image: linear-gradient(to_top,black,transparent)]"></div>
-                <div className="absolute inset-0 shadow-[0_0_10px_rgb(140,69,255,.7)_inset rounded-lg"></div>
-            </div>
-            <span>Join waitlist</span>
-            </button>
+            <Button>Join waitlist</Button>
             <Image className="md:hidden" src={MenuIcon} alt="menu" />
           </div>
         </div>
---
+++ src/components/Hero.tsx
@@ -0,0 +1,20 @@
+import { Button } from "./Button";
+
+export const Hero = () => {
+  return (
+    <section className="h-[492px] flex items-center">
+      <div className="container">
+        <h1 className="text-8xl font-semibold tracking-tighter text-center bg-white bg-[radial-gradient(100%_100%_at_top_left,white,white,rgb(74,32,138,.5))] text-transparent bg-clip-text">
+          AI SEO
+        </h1>
+        <p className="text-lg text-centertext-white/70 my-5">
+          Elevate your site's visibilty effortlessly with AI, where smart
+          technology meets user-friendly SEO tools.
+        </p>
+        <div className="flex justify-center">
+          <Button>Join waitlist</Button>
+        </div>
+      </div>
+    </section>
+  );
+};
