0136754e34caa7675abd1a3888e6156cf8a38d2e
Harshit Bakrania <harshit.bakrania@gmail.com>
2024-11-20 13:39:18 +0530

    logo ticked and backdrop blur to header added

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,8 +6,10 @@ import { Button } from "./Button";
 export const Header = () => {
   return (
     <header className="py-4 border-b border-white/15 md:border-none sticky top-0 z-10">
+      <div className="absolute inset-0 backdrop-blur -z-10 md:hidden"></div>
       <div className="container">
-        <div className="flex justify-between items-center md:border border-white/15 p-2.5 rounded-xl max-w-2xl mx-auto backdrop-blur">
+        <div className="flex justify-between items-center md:border border-white/15 p-2.5 rounded-xl max-w-2xl mx-auto relative">
+          <div className="absolute inset-0 backdrop-blur -z-10 hidden md:block"></div>
           <div>
             <Image className="h-8 w-8" src={LogoIcon} alt="logo" />
           </div>
