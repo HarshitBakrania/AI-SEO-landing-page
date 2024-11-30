cf1f81231176af55bcf392db5bd3355f64bddd9b
Harshit Bakrania <harshit.bakrania@gmail.com>
2024-11-30 22:56:33 +0530

    fixed background color bug

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,7 @@ import { Reviews } from "@/components/Reviews";
 
 export default function Home() {
   return (
-    <div>
+    <div className="bg-[#0A0A0A]">
       <Header />
       <Hero />
       <LogoTicker />
